8efb5cb4b7a390f1a325f7c74e75c165a41983b8
ls1031 <125716107+ls1031@users.noreply.github.com>
2024-02-17 13:31:13 -0800

    Luis first commit

--- GenVN/GenVN.py
+++ GenVN/GenVN.py
@@ -4,7 +4,7 @@ from rxconfig import config
 
 import reflex as rx
 
-import env
+#import env
 
 docs_url = "https://reflex.dev/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
@@ -12,24 +12,45 @@ filename = f"{config.app_name}/{config.app_name}.py"
 
 class State(rx.State):
     """The app state."""
+    prompt = ""
+    image_url = ""
+    processing = False
+    complete = False
+
+    def get_image(self):
+        """Get the image from the prompt."""
+        if self.prompt == "":
+            return rx.window_alert("Prompt Empty")  
+        self.image_url = "/favicon.ico" #Here we can get the api involved to get the image
+        self.prompt = ""
+
+
+def textBox() -> rx.Component:
+     return rx.box(
+        rx.container(
+            rx.card(
+                rx.text_area(value=State.prompt),
+                rx.input(placeholder="Response here",
+                         on_change=State.set_prompt,),
+                width="100%",
+            ),
+            size="4",
+        ),
+        background_color="var(--gray-3)",
+        width="100%",
+    )
+
 
 
 def index() -> rx.Component:
     return rx.center(
-        rx.theme_panel(),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text("Get started by editing ", rx.code(filename)),
-            rx.button(
-                "Check out our docs!",
-                on_click=lambda: rx.redirect(docs_url),
-                size="4",
-            ),
-            align="center",
-            spacing="7",
-            font_size="2em",
+        rx.box(
+            rx.image(src=State.image_url, width="20em"),
         ),
-        height="100vh",
+        textBox(),
+        rx.button("Generate Image", on_click=State.get_image, width="25em"),
+        flex_direction="column",
+        width="100%",
     )
 
 
