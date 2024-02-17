becefddaf717f10d4993b682893b439cfcc88654
MatthewMattei <96449493+MatthewMattei@users.noreply.github.com>
2024-02-17 13:01:38 -0800

    Added in model API request info

--- GenVN/GenVN.py
+++ GenVN/GenVN.py
@@ -1,18 +1,48 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 from rxconfig import config
-
 import reflex as rx
-
+from monsterapi import client
+import os
 import env
 
 docs_url = "https://reflex.dev/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
+monster_client = client(os.environ["MONSTER_API_KEY"])
+models = {"text": 'llama2-7b-chat', "image": 'sdxl-base', "img_mod": 'photo-maker'}
+input_data = {
+"text": {
+  'prompt': 'Whats the meaning of life?',
+  'top_k': 10,
+  'top_p': 0.9,
+  'temp': 0.9,
+  'max_length': 1000,
+  'beam_size': 1,
+  'system_prompt': 'You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe...',
+  'repetition_penalty': 1.2,
+},
+"img": {
+  'prompt': 'HD photo of an astronaut with the face of a tiger, sitting in a forest, looking at the sunset, surreal',
+  'negprompt': 'unreal, fake, meme, joke, disfigured, poor quality, bad, ugly',
+  'samples': 2,
+  'steps': 50,
+  'aspect_ratio': 'square',
+  'guidance_scale': 7.5,
+  'seed': 2414,
+},
+"img_mod": {
+  "prompt": "a man wearing a leather jacket",
+  "init_image_url": "www.example.com/image_jpeg",
+  "negprompt": "deformed, bad anatomy, disfigured, poorly drawn face",
+  "steps": 40,
+  "samples": 1,
+  "strength": 40,
+  "seed": 2414, 
+}}
 
 
 class State(rx.State):
     """The app state."""
-
 
 def index() -> rx.Component:
     return rx.center(
